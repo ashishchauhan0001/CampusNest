62f3ddde17ac408a0df38dc44b6511d6a80767c5
AshishChauhan0001 <asch20080@gmail.com>
2024-10-07 20:22:18 +0530

    ProfilePhoto section updated

---
+++ api/models/vendor.model.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+const vendorSchema = new mongoose.Schema(
+  {
+    Address1: {
+      type: String,
+      required: true,
+    },
+    Rent: {
+        type: Number,
+        required: true,
+      },
+    Address2: {
+      type: String,
+    },
+    Security: {
+      type: Number,
+      required: true,
+    },
+    City: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
