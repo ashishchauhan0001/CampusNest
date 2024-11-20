b8c2b2b724f1fad67f2a73e799271ea814c517a1
AshishChauhan0001 <asch20080@gmail.com>
2024-11-20 14:17:01 +0530

    fixes issues

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ import cors from 'cors';
 dotenv.config()
 
 
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO)
@@ -37,8 +38,8 @@ app.use(cors());
 app.use(cookieParser());
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(PORT, () => {
+  console.log('Server is running on port !',{PORT});
 });
 
 app.use('/api/user', userRouter);
